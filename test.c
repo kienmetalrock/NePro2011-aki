c2e3a16bf9a88e2ee9e86e68305f782c4057a7cb
Chicken at Keio <t11506td@sfc.keio.ac.jp>
2011-11-25 15:48:56 +0900

    test

--- test.c
+++ test.c
@@ -1,13 +1,3 @@
 #include <iostream>
 
-<<<<<<< HEAD
-int main(){
-	cout << "hello !";
-	return 0;
-}
-=======
-
-nlalgnahgowhgnlknalknlgnwa:w
-
-
->>>>>>> 25e19ec16fb5a481fc306b915dd79bf6ea3b7a80
+vim ngu dan
